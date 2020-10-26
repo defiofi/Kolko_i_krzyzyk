7ff482d239bf6181eaa7f0d9f19c8fe986cfcd16
Mariusz Kok <kokmar76@gmail.com>
2020-10-26 20:45:11 +0100

    Tic tac toe game - version 1.01

--- src/main/java/kolkoIKrzyzyk/Grafika/InformationProgram.java
+++ src/main/java/kolkoIKrzyzyk/Grafika/InformationProgram.java
@@ -19,7 +19,7 @@ public class InformationProgram {
         text1.setWrapText(true);
         text1.setEditable(false);
         text1.setFont(Font.font(18));
-        text1.setText("Program Kolko i Krzyzyk - wersja 1.0 \nAplikacja  powstala w pazdzierniku 2020 roku. Tworzona byla w jezyku programowania: java w wersji 11.0.2 oraz przy uzyciu biblioteki javafx. Aplikacja tworzona byla bez uzycia polskiej czcionki i dlatego w polskich slowach nie ma liter z \"ogonkami\".\nProgram umozliwia gre jak w tytule. Gra toczy sie na planszy o 9 polach (3 x 3). Gracz moze ustawic stopien trudnosci, znak ktorym gra oraz kto ma pierwszy ruch. Mozna rowniez ustawic styl planszy i figur. W programie uzyto kilka darmowych grafik ze strony: https://pixabay.com/pl/ \nAutor zyczy wszystkim graczom duzo radosci z uzytkowania programu. ");
+        text1.setText("Program Kolko i Krzyzyk - wersja 1.01 \nAplikacja  powstala w pazdzierniku 2020 roku. Tworzona byla w jezyku programowania: java w wersji 11.0.2 oraz przy uzyciu biblioteki javafx. Aplikacja tworzona byla bez uzycia polskiej czcionki i dlatego w polskich slowach nie ma liter z \"ogonkami\".\nProgram umozliwia gre jak w tytule. Gra toczy sie na planszy o 9 polach (3 x 3). Gracz moze ustawic stopien trudnosci, znak ktorym gra oraz kto ma pierwszy ruch. Mozna rowniez ustawic styl planszy i figur. W programie uzyto kilka darmowych grafik ze strony: https://pixabay.com/pl/ \nAutor zyczy wszystkim graczom duzo radosci z uzytkowania programu. ");
         text1.setMaxSize(720,300);
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
